d1699b8e6e57c3ebf0d6ca1a6848377052fac30f
MirasBurambayev <you@example.com>
2025-02-01 02:21:22 +0500

    lab2

---
+++ LAB2/list/RemoveListItems.py
@@ -0,0 +1,7 @@
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+del thislist
+#thislist = ["apple", "banana", "cherry"]
+#thislist.clear()
+print(thislist)
